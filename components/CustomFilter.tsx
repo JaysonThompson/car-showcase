5f7dbbb4b39f92c14dda398b0fadf9b028b8c74d
Jayson Thompson <jaysonthompson922@gmail.com>
2023-09-26 13:01:01 -0700

    adds static cars for catalogue

---
+++ components/CustomFilter.tsx
@@ -0,0 +1,11 @@
+import { CustomFilterProps } from "@/types";
+
+const CustomFilter = ({
+	title,
+	containerStyles,
+	handleClick,
+}: CustomFilterProps) => {
+	return <div>CustomFilter</div>;
+};
+
+export default CustomFilter;
