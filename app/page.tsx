a41a32e06fed502a3f73f92ab7d271f098c58d96
Jayson Thompson <jaysonthompson922@gmail.com>
2023-09-28 08:33:08 -0700

    adds filters and view more feature

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 import Image from "next/image";
 
-import { Hero, SearchBar, CustomFilter, CarCard } from "@/components";
+import { Hero, SearchBar, CustomFilter, CarCard, ShowMore } from "@/components";
 import { fetchCars } from "@/utils";
 import { fuels, yearsOfProduction } from "@/constants";
 
@@ -44,6 +44,10 @@ export default async function Home({ searchParams }: HomeProps) {
 								<CarCard car={car} />
 							))}
 						</div>
+						<ShowMore
+							pageNumber={(searchParams.limit || 10) / 10}
+							isNext={(searchParams.limit || 10) > allCars.length}
+						/>
 					</section>
 				) : (
 					<div className="home__error-container">
--- components/CustomFilter.tsx
+++ components/CustomFilter.tsx
@@ -6,12 +6,31 @@ import { CustomFilterProps } from "@/types";
 import { Listbox, Transition } from "@headlessui/react";
 
 import Image from "next/image";
+import { updateSearchParams } from "@/utils";
 
 const CustomFilter = ({ title, options }: CustomFilterProps) => {
 	const [selected, setSelected] = useState(options[0]);
+
+	const router = useRouter();
+
+	const handleUpdateParams = (event: { title: string; value: string }) => {
+		const newPathName = updateSearchParams(
+			title,
+			event.value.toLowerCase()
+		);
+
+		router.push(newPathName);
+	};
+
 	return (
 		<div className="w-fit">
-			<Listbox value={selected} onChange={(event) => setSelected(event)}>
+			<Listbox
+				value={selected}
+				onChange={(event) => {
+					setSelected(event);
+					handleUpdateParams(event);
+				}}
+			>
 				<div className="relative w-fit z-10">
 					<Listbox.Button className="custom-filter__btn">
 						<span className="block truncate">{selected.title}</span>
@@ -28,7 +47,35 @@ const CustomFilter = ({ title, options }: CustomFilterProps) => {
 						leave="transition ease-in duration-100"
 						leaveFrom="opacity-100"
 						leaveTo="opacity-0"
-					></Transition>
+					>
+						<Listbox.Options className="custom-filter__options">
+							{options.map((option) => (
+								<Listbox.Option
+									key={option.title}
+									value={option}
+									className={({ active }) =>
+										`relative cursor-default select-none py-2 px-4 ${
+											active
+												? "bg-primary-blue text-white"
+												: "text-gray-900"
+										}`
+									}
+								>
+									{({ selected }) => (
+										<span
+											className={`block truncate ${
+												selected
+													? "font-medium"
+													: "font-normal"
+											}`}
+										>
+											{option.title}
+										</span>
+									)}
+								</Listbox.Option>
+							))}
+						</Listbox.Options>
+					</Transition>
 				</div>
 			</Listbox>
 		</div>
